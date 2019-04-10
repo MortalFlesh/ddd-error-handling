b07771807dc5e03db837952655e00d71fd24e25a
Petr Chromec <mortal_flesh@seznam.cz>
2019-04-10 10:06:42 +0200

    Add divide integers function to solve the problem

--- src/CompanyDomain.fs
+++ src/CompanyDomain.fs
@@ -11,8 +11,10 @@ type DivideIntegersAsAService = UserInput -> int
 module DivideInts =
     type private DivideIntegers = int -> int -> int
 
+    let divideIntegers: DivideIntegers = (/)
+
     let byUserInput log : DivideIntegersAsAService =
         fun { Mathematician = mathematician; Divider = divider; Divisior = divisor } ->
             log (sprintf "I'm counting %s / %s for %s" divider divisor mathematician)
 
-            failwithf "Not implemented yet."
+            divideIntegers (int divider) (int divisor)
