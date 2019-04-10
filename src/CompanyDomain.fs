89a98bf590f363d608af86f8b4de1f3d3b1b7b6f
Petr Chromec <mortal_flesh@seznam.cz>
2019-04-10 11:01:58 +0200

    Allow Chuck Norris to divide by zero

--- src/CompanyDomain.fs
+++ src/CompanyDomain.fs
@@ -8,6 +8,7 @@ type UserInput = {
 
 type DivisionResult =
     | Int of int
+    | OnlyChuckNorrisKnows
 
 type ParseError =
     | NotANumberError of string
@@ -15,6 +16,7 @@ type ParseError =
 type DivisionError =
     | DividerError of DividerError
     | DivisorError of DivisorError
+    | UnsupportedDivisionError
 
 and DividerError =
     | ParseError of ParseError
@@ -29,12 +31,24 @@ module DivideInts =
     //
     // domain private types
     //
+    type Mathematician =
+        | Common of string
+        | ChuckNorris
+
+    module private Mathematician =
+        let value = function
+            | Common name -> name
+            | ChuckNorris -> "🤜  Chuck Norris 🤛"
+
     type private Divider =
         | Int of int
 
     type private Divisor =
         | NotZeroInt of int
+        | ZeroInt
 
+    type private DomainDivide = Mathematician -> Divider -> Divisor -> Result<DivisionResult, DivisionError>
+    type private CommonDivide = Divider -> Divisor -> Result<DivisionResult, DivisionError>
     type private DivideIntegers = int -> int -> int
 
     //
@@ -43,13 +57,36 @@ module DivideInts =
 
     let private divideIntegers: DivideIntegers = (/)
 
-    let private divide divider divisor =
-        match (divider, divisor) with
-        | Divider.Int divider, Divisor.NotZeroInt divisor -> divideIntegers divider divisor |> DivisionResult.Int |> Ok
+    let private commonDivide: CommonDivide =
+        fun divider divisor ->
+            match (divider, divisor) with
+            | _, Divisor.ZeroInt -> DivisionByZero |> DivisorError |> Error
+            | Divider.Int divider, Divisor.NotZeroInt divisor -> divideIntegers divider divisor |> DivisionResult.Int |> Ok
+            //| _ -> UnsupportedDivisionError |> Error
+
+    let private divide: DomainDivide =
+        fun mathematican divider divisor ->
+            match mathematican with
+            | Common _ -> commonDivide divider divisor
+            | ChuckNorris ->
+                match commonDivide divider divisor with
+                | Ok result -> Ok result
+                | Error e ->
+                    match e with
+                    | DivisorError e ->
+                        match e with
+                        | DivisionByZero -> Ok OnlyChuckNorrisKnows
+                        | _ -> Error (DivisorError e)
+                    | _ -> Error e
 
     //
     // parsing
     //
+
+    let private (|ChuckNorrisName|_|) = function
+        | "Chuck Norris"
+        | "ChuckNorris" -> Some ChuckNorrisName
+        | _ -> None
 
     let private (|Int|_|) (string: string) =
         match System.Int32.TryParse(string) with
@@ -62,9 +99,9 @@ module DivideInts =
         | input -> input |> NotANumberError |> DividerError.ParseError |> Error
 
     let private parseDivisor = function
-        | Int intNumber when intNumber = 0 -> Error (DivisorError.DivisionByZero)
-        | Int intNumber -> Ok (Divisor.NotZeroInt intNumber)
-        | input ->  input |> NotANumberError |> DivisorError.ParseError |> Error
+        | Int intNumber when intNumber = 0 -> Ok ZeroInt
+        | Int intNumber -> Ok (NotZeroInt intNumber)
+        | input -> input |> NotANumberError |> DivisorError.ParseError |> Error
 
     //
     // public api
@@ -72,7 +109,12 @@ module DivideInts =
 
     let byUserInput log : DivideIntegersAsAService =
         fun { Mathematician = mathematicianInput; Divider = dividerInput; Divisior = divisorInput } ->
-            log (sprintf "I'm counting %s / %s for %s" dividerInput divisorInput mathematicianInput)
+            let mathematican =
+                match mathematicianInput with
+                | ChuckNorrisName -> ChuckNorris
+                | _ -> Common mathematicianInput
+
+            log (sprintf "I'm counting %s / %s for %s" dividerInput divisorInput (mathematican |> Mathematician.value))
 
             result {
                 let! divider =
@@ -85,5 +127,5 @@ module DivideInts =
                     |> parseDivisor
                     |> Result.mapError DivisorError
 
-                return! divide divider divisor
+                return! divide mathematican divider divisor
             }
